6319a2a664e5fad94cbbfacd2e536f93d9a2facc
Mohamed Ali Larbi Daho Bachir <alildb2036@gmail.com>
2022-08-02 17:23:06 -0400

    Initial Commit

---
+++ BitcoinReporter/BitcoinReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinReporterApp.swift
+//  BitcoinReporter
+//
+//  Created by Ali Daho on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
